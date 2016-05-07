298403639490afc71b88762c01f2602b153bb192
Tony Lai <a.lai@berkeley.edu>
2016-05-07 16:40:28 -0700

    fdafdsa

--- PennyPop/GameScene.swift
+++ PennyPop/GameScene.swift
@@ -10,15 +10,60 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    // 1
     let player = SKSpriteNode(imageNamed: "player")
     
     override func didMoveToView(view: SKView) {
-        // 2
         backgroundColor = SKColor.whiteColor()
-        // 3
-        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-        // 4
+        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         addChild(player)
+        addMonster()
+        addMonster()
+        addMonster()
+        addMonster()
+        addMonster()
+        
+//        runAction(SKAction.repeatActionForever(
+//            SKAction.sequence([
+//                SKAction.runBlock(addMonster),
+//                SKAction.waitForDuration(0.4)
+//                ])
+//            ))
     }
+    
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        var randY = CGFloat(0.5)
+        while (randY > CGFloat(0.45) && randY < CGFloat(0.55)) {
+            randY = random(min: 0.1, max: 0.9)
+        }
+        var randX = CGFloat(0.5)
+        while (randX > CGFloat(0.45) && randX < CGFloat(0.55)) {
+            randX = random(min: 0.1, max: 0.9)
+        }
+        
+        monster.position = CGPoint(x: randX * size.width, y: randY * size.height)
+        
+        // Add the monster to the scene
+        addChild(monster)
+        
+        // Determine speed of the monster
+//        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        // Create the actions
+//        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+//        let actionMoveDone = SKAction.removeFromParent()
+//        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
+    }
+    
 }
